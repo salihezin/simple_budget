163ac5a398eaf5df3dc27656652fcff4d07870a5
salihezin <salihezin@gmail.com>
2023-11-18 19:23:40 +0300

    add: added login check for home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,19 @@
+import React, {useEffect} from 'react';
+import {useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
 const Home = () => {
+    const auth = useSelector(state => state.auth);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!auth.user) {
+            navigate('/');
+        }
+    }, [auth, navigate]);
+
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>Home Page {auth.userId}</h1>
         </div>
     )
 }
